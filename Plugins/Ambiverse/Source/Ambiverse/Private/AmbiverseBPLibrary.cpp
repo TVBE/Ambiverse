9b67b8ed83bd9dbfba94669edf18081013dcfac0
Tim Verberne <timverbe@gmail.com>
2023-05-14 17:13:56 +0200

    project setup

---
+++ Plugins/Ambiverse/Source/Ambiverse/Private/AmbiverseBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AmbiverseBPLibrary.h"
+#include "Ambiverse.h"
+
+UAmbiverseBPLibrary::UAmbiverseBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UAmbiverseBPLibrary::AmbiverseSampleFunction(float Param)
+{
+	return -1;
+}
+
---
+++ Source/AmbiversePlugin/AmbiversePluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AmbiversePluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMBIVERSEPLUGIN_API AAmbiversePluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
